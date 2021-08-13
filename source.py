12908771f7dfcc204d00d3719ff01c1739a6d7cb
v4rts <v4rts@yandex.ru>
2021-08-13 12:56:33 +0300

    resuming work

--- source.py
+++ source.py
@@ -6,8 +6,8 @@ from download import *
 from rotation import *
 from diagnostic import *
 
-host = 'v4rts.beget.tech'
-user = 'v4rts_test'
+host = 'v4rts2.beget.tech'
+user = 'v4rts2_test'
 password = '1234Bb!'
 savePathPrimary = '/Users/macuser/desktop/FTP-FileMonitoring/primaryfiles'
 savePathChecksum = '/Users/macuser/desktop/FTP-FileMonitoring/checksumFiles'
